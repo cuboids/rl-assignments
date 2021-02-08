a235396f45b5ac9881849f394a0950c7d2447f72
cuboids <44651818+cuboids@users.noreply.github.com>
2021-02-08 21:02:09 +0100

    Update alpha-beta.py
    Wrote a function that creates hexagonal grids :)

--- alpha-beta.py
+++ alpha-beta.py
@@ -1,1 +1,12 @@
+import re
 
+
+def make_grid(nrow=11, ncol=None):
+    """Prints hexagonal grid of dimensions nrow * ncol"""
+    
+    if ncol is None:
+        ncol = nrow
+    row = (ncol + 1) * " \__/ "
+    print(re.sub(r"\\|/", " ", row)[7:])
+    for i in range(-4, 3 * nrow - 1, 3):
+        print(max(0, i) * " " + row[max(0, -i):min(-2, 3 * nrow - 11 - i)])
