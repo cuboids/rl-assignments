6ab77ab74a1ca2f58e71490880a98c544b30261d
cuboids <luminouscuboids@pm.me>
2021-02-10 21:21:39 +0100

    Rewrote hex_skeleton and skeleton_usage to be a bit more clear.

--- skeleton_usage.py
+++ skeleton_usage.py
@@ -2,29 +2,31 @@ import numpy as np
 from hex_skeleton import HexBoard
 
 # sanity check that wins are detected
-for i in range(0,2):
-  winner = HexBoard.RED if i == 0 else HexBoard.BLUE
-  loser = HexBoard.BLUE if i == 0 else HexBoard.RED
-  board = HexBoard(3)
-  board.place((1,1), loser)
-  board.place((2,1), loser)
-  board.place((1,2), loser)
-  board.place((2,2), loser)
-  board.place((0,0), winner)
-  board.place((1,0), winner)
-  board.place((2,0), winner)
-  board.place((0,1), winner)
-  board.place((0,2), winner)
-  assert(board.check_win(winner) == True)
-  assert(board.check_win(loser) == False)
-  board.print()
+for i in range(2):
+    winner = HexBoard.RED if i == 0 else HexBoard.BLUE
+    loser = HexBoard.BLUE if i == 0 else HexBoard.RED
+    board = HexBoard(3)
+    board.place((1,1), loser)
+    board.place((2,1), loser)
+    board.place((1,2), loser)
+    board.place((2,2), loser)
+    board.place((0,0), winner)
+    board.place((1,0), winner)
+    board.place((2,0), winner)
+    board.place((0,1), winner)
+    board.place((0,2), winner)
+    assert board.check_win(winner)
+    assert not board.check_win(loser)
+    board.print()
+
 endable_board = HexBoard(4)
+
 # sanity check that random play will at some point end the game
 while not endable_board.game_over:
-  endable_board.place((np.random.randint(0, 4), np.random.randint(0, 4)), HexBoard.RED)
-assert(endable_board.game_over == True)
-assert(endable_board.check_win(HexBoard.RED) == True)
-assert(endable_board.check_win(HexBoard.BLUE) == False)
+    endable_board.place((np.random.randint(0, 4), np.random.randint(0, 4)), HexBoard.RED)
+assert endable_board.game_over
+assert endable_board.check_win(HexBoard.RED)
+assert not endable_board.check_win(HexBoard.BLUE)
 print("Randomly filled board")
 endable_board.print()
 
@@ -47,9 +49,9 @@ assert(neighbor_check_small.get_neighbors((1, 1)) == [(0, 1), (1, 0)])
 
 neighbor_check_sanity = HexBoard(11)
 for x in range(0, 11):
-  for y in range(0, 11):
-    neighbors = neighbor_check_sanity.get_neighbors((x, y))
-    for neighbor in neighbors:
-      neighbors_neighbors = neighbor_check_sanity.get_neighbors(neighbor)
-      index_of_self = neighbors_neighbors.index((x, y))
-      assert(index_of_self != -1)
+    for y in range(0, 11):
+        neighbors = neighbor_check_sanity.get_neighbors((x, y))
+        for neighbor in neighbors:
+            neighbors_neighbors = neighbor_check_sanity.get_neighbors(neighbor)
+            index_of_self = neighbors_neighbors.index((x, y))
+            assert(index_of_self != -1)
