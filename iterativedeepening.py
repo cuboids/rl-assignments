de9c48371342dc0b72b9c93f208815445e9384c8
choihungzoeker <fyodor.feng@gmail.com>
2021-02-21 18:01:49 +0800

    Update iterativedeepening.py
    First version of iterative deepening. Further testing required.

--- iterativedeepening.py
+++ iterativedeepening.py
@@ -1,7 +1,7 @@
 import time
-from alphabeta_noprint import alphabeta
-from printTree import printTree
 import numpy as np
+from ttalphabeta import ttalphabeta
+from transpositiontable import TranspositionTable
 
 def iterativedeepening(board, timelimit, ntype, p, a=-np.inf, b=np.inf):
     """
@@ -10,7 +10,7 @@ def iterativedeepening(board, timelimit, ntype, p, a=-np.inf, b=np.inf):
     EITHER 1) kernel is interuppted OR 2) timeout OR 3) search depth exceeds board empty positions.
     Parameters: 
         board (HexBoard object):
-        timelimit (int): search time limit in seconds
+        timelimit (int): search time limit in seconds. SUGGEST testing with timelimit from 1 first
         ntype (str): carry to alphabeta(), refer to alphabeta() docstring
         p (int): carry to alphabeta(), refer to alphabeta() docstring
         a (float): carry to alphabeta(), refer to alphabeta() docstring
@@ -19,29 +19,35 @@ def iterativedeepening(board, timelimit, ntype, p, a=-np.inf, b=np.inf):
         node (dict): {'state', 'depth', 'children', 'type', 'score', 'move'}
     """
     # Initialize
-    timeout = time.time() + timelimit # Define timeout criteria
-    depth = 1 # Start with shallow depth
-    print('Interrupt the kernel to terminate search.\n')
+    timeout = time.time() + timelimit  # Define timeout criteria
+    depth = 1  # Start with shallow depth
+    tt = TranspositionTable()  # Initialize search with empty
+    print('USER NOTE: Interrupt the kernel to terminate search. \n')
     
     try:
         while True:
-            print(f'\n[In progress] Start iteration at depth {depth}')
-            n = alphabeta(board, depth, ntype, p, a, b)
-            print(f'[In progress] Finish iteration at depth {depth}: Best move = {n["move"]}')
-            #print('\nprint tree:') # For checking
-            #printTree(n) # For checking
-            if time.time() > timeout: # This method is not perfect, may change to raise + class Exception for instant interrupt
-                print('\nSearch terminated: TIMEOUT')
+            print(f'[Iteration status] Start iteration at depth {depth} \n')
+            # Option: alpha-beta + TT
+            (result_node, tt) = ttalphabeta(board=board, depth=depth, ntype=ntype, p=p, a=a, b=b, tt=tt)  # Use TT from previous search to improve efficiency
+            # Alternative option: alpha-beta
+            #n = alphabeta(board, depth, ntype, p, a, b)
+            print(f'[Iteration status] Finish iteration at depth {depth}:', end=" ")
+            print(f'Best move at root node = {result_node["move"]} \n')
+            if time.time() > timeout:  # WARNING This method is not perfect and only breaks after search completed, may change to raise + class Exception for instant interrupt
+                print('[Iteration status] Termination: TIMEOUT \n')
+                print(f'[Iteration report] Return result of completed search at depth {depth} \n')
                 break
             if depth == len(board.get_allempty()):
-                print('\nSearch terminated: FULL DEPTH SEARCH IS COMPLETED')
+                print('[Iteration status] Termination: EXACT SEARCH')
+                print(f'[Iteration report] Return result of completed search at depth {depth} \n')
                 break
-            depth += 1 # Increase depth after one iteration
+            depth += 1  # Increase depth after one iteration
         
-    except KeyboardInterrupt: # Interrupt kernel, Ctrl+c in console
-        print('\nSearch terminated: INTERRUPT')
+    except KeyboardInterrupt:  # Interrupt kernel, Ctrl+c in console
+        print('[Iteration status] Termination: USER INTERRUPT')
+        print(f'[Iteration report] Return result of completed search at depth {depth-1} \n')
         pass
     
     finally:
-        print(f'\nIterative deepening stopped at depth {depth}')
-        return n
+        return result # Normal output for repeat games, TT not required in this case.
+        #return (result_node, tt)  # Return for test only. Conflict with repeat games expected.
