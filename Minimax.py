0ff78f87e4264cde63f0134b301d00002f8d3ce1
choihungzoeker <fyodor.feng@gmail.com>
2021-02-23 00:05:42 +0800

    Update Minimax.py
    [minor change]
    improve argument input

--- Minimax.py
+++ Minimax.py
@@ -5,7 +5,7 @@ import copy
 import random
 
 
-def minimax(board, depth, ntype, p):
+def minimax(board, depth, p, ntype='MAX'):
     """
     Minimax function, depth-limited
     Parameters: 
@@ -43,17 +43,15 @@ def minimax(board, depth, ntype, p):
     
     # Main loop
     if n['state'].is_game_over():  # Case: gameover at depth >= 0 (do we need to give bonus or penalty to score?)
-        n['type'] = 'LEAF_ENDGAME'
-        n['score'] = random.sample(range(0, 10), 1)[0]
-        # n['score'] = eval(n['state'])
-        print(' Leaf SCORE (ENDGAME) =', n['score'], '\n')
+        n['type'] = 'LEAF'
+        n['score'] = evaluateScore(n['state'], 'random')
+        print(' Leaf SCORE (LEAF) =', n['score'], '\n')
         return n
     
     elif depth == 0:  # Case: reaching the search tree depth limit
-        n['type'] = 'LEAF_HEURISTIC'
-        n['score'] = random.sample(range(0, 10), 1)[0]
-        # n['score'] = eval(n['state'])
-        print(' Leaf SCORE (HEURISTIC) =', n['score'], '\n')
+        n['type'] = 'DEPTH==0'
+        n['score'] = evaluateScore(n['state'], 'random')
+        print(' Leaf SCORE (DEPTH==0) =', n['score'], '\n')
         return n
     
     elif n['type'] == 'MAX':  # Max node
@@ -70,7 +68,7 @@ def minimax(board, depth, ntype, p):
             new_state.print()  # Eyetest child state
             new_p = [1, 2]
             new_p.remove(p)  # Reverse persective for child node
-            child_n = minimax(new_state, n['depth'] - 1, 'MIN', new_p[0])  # Generate child node
+            child_n = minimax(new_state, n['depth'] - 1, new_p[0], 'MIN')  # Generate child node
             n['children'].update({str(child_move): child_n})  # Store children node
             if child_n['score'] > g_max:  # Update current node to back up from the maximum child node
                 g_max = child_n['score']
@@ -93,7 +91,7 @@ def minimax(board, depth, ntype, p):
             new_state.print()
             new_p = [1, 2]
             new_p.remove(p)  # Reverse persective for child node
-            child_n = minimax(new_state, n['depth'] - 1, 'MAX', new_p[0])
+            child_n = minimax(new_state, n['depth'] - 1, new_p[0], 'MAX')
             n['children'].update({str(child_move): child_n})  # Store children node
             if child_n['score'] < g_min:  # Update current node to back up from the minimum child node
                 g_min = child_n['score']
