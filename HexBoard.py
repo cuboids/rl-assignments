4719d5c7fb7e8d74f8c26713d8122b188b6b9128
cuboids <luminouscuboids@pm.me>
2021-02-24 00:38:18 +0100

    Major changes in both HexBoard and RepeatGames

--- HexBoard.py
+++ HexBoard.py
@@ -1,9 +1,22 @@
-# Hex board skeleton + .get_allempty()
+import copy
 import names  # For giving random names to agents. See https://pypi.org/project/names/
+import numpy as np
+import random
 from trueskill import Rating, rate_1vs1
+# from chooseEval import evaluateScore  # TO BE DEPRECIATED
+
+
+class Human:
+    # Should we make a separate class for humans?
+    pass
 
 
 class Agent:
+
+    # Set DEBUG to True if you want to debug.
+    # WARNING: this will print way too much.
+    DEBUG = False
+
     def __init__(self, name=None, depth=3, searchby="random", timelimit=2):
         """Sets up the agent.
 
@@ -18,10 +31,20 @@ class Agent:
         else:
             self.name = name
         self.depth = depth
-        self.timelimit = timelimit
-        self.searchby = searchby
         self.rating = Rating()
         self.rating_history = [self.rating]
+        self.searchby = searchby
+        self.timelimit = timelimit
+        self.color = None
+
+    def set_color(self, col):
+        """Gives agent a color
+
+        Args:
+            col: Either HexBoard.BLUE or HexBoard.RED
+            """
+        if col in (1, 2):
+            self.color = col
 
     def rate_1vs1(self, opponent, opponent_won=False):
         """Updates the rating of agent and opponent after playing a 1vs1
@@ -36,6 +59,186 @@ class Agent:
         else:
             self.rating, opponent.rating = rate_1vs1(rating2, self.rating)
         self.rating_history.append(self.rating)
+
+    def make_move(self, game):
+        """Let's the agent calculate a move based on it's searchby strategy
+
+        Args:
+            game: position of type HexBoard.
+        """
+        return eval('self.' + self.searchby + '(game)')['move']
+
+    @staticmethod
+    def random(game):
+        """Let the agent make a random move"""
+        return {'move': random.sample(game.get_allempty(), 1)[0]}
+
+    def minimax(self, game, depth=None, ntype=None, p=None):
+        """
+        Let the agent make a depth-limited minimax move
+
+        Args:
+            game: A HexBoard instance.
+            depth (int): depth limit of search tree, if depth exceeds empty positions, it will be reduced
+            ntype (str): node type, either 'MAX' or 'MIN'
+            p (int): perspective/player of search tree root, either 1 for HexBoard.BLUE, or 2 for HexBoard.RED
+
+        Returns:
+            A dict including state, depth, children, type, score, and move.
+
+        Further improvements:
+            search statistics: nodes searched + cutoffs
+        """
+
+        # Movelist for current state
+        movelist = game.get_allempty()
+        if depth is ntype is p is None:
+            depth, ntype, p = self.depth, "MAX", self.color
+
+        # For small board and end game, depth limit == full depth
+        if depth > len(movelist):
+            print('WARNING: DEPTH is limited by empty positions in board => set to full depth search.\n')
+            depth = len(movelist)
+
+        # Initialize node
+        n = {'state': game, 'depth': depth, 'children': {}, 'type': ntype}
+
+        if self.DEBUG:
+            print(f'\nNode DEPTH = {n["depth"]} (TYPE = {n["type"]})')
+            print(' GAME OVER?', n['state'].game_over)
+            if depth and not n['state'].is_game_over():
+                print(f' PLAYER {p} to consider EMPTY positions {movelist}')
+
+        # Initialize child_count to count children at depth d
+        child_count = 0
+
+        # Main loop
+        if n['state'].is_game_over():  # Case: gameover at depth >= 0 (do we need to give bonus or penalty to score?)
+            n['type'] = 'LEAF'
+            n['score'] = np.inf if n['state'].check_win(self.color) else -np.inf
+            if self.DEBUG:
+                print(' Leaf SCORE (LEAF) =', n['score'], '\n')
+            return n
+
+        elif not depth:  # Case: reaching the search tree depth limit
+            n['type'] = 'DEPTH==0'
+            n['score'] = self.eval_dijkstra2(n['state'])
+            if self.DEBUG:
+                print(' Leaf SCORE (DEPTH==0) =', n['score'], '\n')
+            return n
+
+        elif n['type'] == 'MAX':  # Max node
+            g_max = -np.inf  # Initialize max score with very small
+            n['score'] = g_max
+            for child_move in movelist:  # Search all children and compare score
+                child_count += 1
+                if self.DEBUG:
+                    print(f'\nFrom DEPTH {n["depth"]} branch --> Child {child_count}:')
+                    print(f'\nPLAYER {p} moves as {child_move} STATE before move:')
+                    n['state'].print()
+                new_state = copy.deepcopy(n['state'])  # Copy state to avoid modifying current state
+                new_state.place(child_move, p)  # Generate child state
+                if self.DEBUG:
+                    print(' STATE after move:')
+                    new_state.print()  # Eyetest child state
+                child_n = self.minimax(new_state, n['depth'] - 1, 'MIN', p)  # Generate child node
+                n['children'].update({str(child_move): child_n})  # Store children node
+                if child_n['score'] > g_max:  # Update current node to back up from the maximum child node
+                    g_max = child_n['score']
+                    n['score'] = child_n['score']
+                    n['move'] = child_move
+                if self.DEBUG:
+                    print(f'End of PLAYER {p} DEPTH {n["depth"]} {n["type"]} node:', end='')
+                    print(f'Child move {child_move}', end=' ')
+                    print(f'score = {child_n["score"]}; Updated optimal move {n["move"]} score = {n["score"]}.')
+
+        elif n['type'] == 'MIN':  # Min node
+            g_min = np.inf  # Initialize min score with very large
+            n['score'] = g_min
+            for child_move in movelist:
+                child_count = child_count + 1
+                if self.DEBUG:
+                    print(f'\nFrom DEPTH {n["depth"]} branch --> Child {child_count}:')
+                    print(f'PLAYER {p} moves at {child_move} STATE before move:')
+                    n['state'].print()
+                new_p = [1, 2]
+                new_p.remove(p)  # Reverse perspective for child node
+                new_state = copy.deepcopy(n['state'])
+                new_state.place(child_move, new_p[0])  # Generate child state
+                if self.DEBUG:
+                    print(' STATE after move:')
+                    new_state.print()
+
+                child_n = self.minimax(new_state, n['depth'] - 1, 'MAX', p)
+                n['children'].update({str(child_move): child_n})  # Store children node
+                if child_n['score'] < g_min:  # Update current node to back up from the minimum child node
+                    g_min = child_n['score']
+                    n['score'] = child_n['score']
+                    n['move'] = child_move
+                if self.DEBUG:
+                    print(f'End of PLAYER {p} DEPTH {n["depth"]} {n["type"]} node: Child move {child_move}', end=" ")
+                    print(f'score = {child_n["score"]}; Updated optimal move {n["move"]} score = {n["score"]}.')
+        else:
+            print('Error: Nothing to execute.')
+            return
+
+        return n  # g is the maximun heuristic function value
+
+    def alphabeta(self, game):
+        """Let the agent make an alphabeta move"""
+        pass
+
+    def alphabetaIDTT(self, game):
+        """Let the agent make an alphabeta move with IT and TTs"""
+        pass
+
+    def dijkstra(self, game):
+        """Evaluate position with the Dijkstra algorithm"""
+        return
+
+    def human(self, game):
+        """Should humans be a type of agent or a separate class?"""
+        pass
+
+    def eval_dijkstra2(self, game):
+        return (-1)**self.color * (self.dijkstra2(game, 1) - self.dijkstra2(game, 2))
+
+    @staticmethod
+    def dijkstra2(game, player):
+        """Evaluate position with Robbie's Dijkstra algorithm"""
+
+        if player == HexBoard.BLUE:
+            source, destination, ignore1, ignore2 = 'Left', 'Right', 'Top', 'Down'
+        else:
+            source, destination, ignore1, ignore2 = 'Top', 'Down', 'Left', 'Right'
+        distance = {k: np.inf for k in game.get_all()}
+        distance.update({source: 0, destination: np.inf})
+        unvisited = {k: True for k in game.get_all()}
+        unvisited.update({source: True, destination: True, ignore1: False, ignore2: False})
+        square = source
+
+        def dijkstra2_r(game, player, square, distance, unvisited, destination):
+            """ This is the recursive part of the algorithm"""
+
+            # Update distances for neighbors
+            for neighbor in game.get_neighbors(square, extra_hexes=True):
+                if unvisited[neighbor]:
+                    color = game.get_color(neighbor)
+                    if color == player:
+                        distance[neighbor] = min(distance[neighbor], distance[square])
+                    elif color == HexBoard.EMPTY:
+                        distance[neighbor] = min(distance[neighbor], distance[square] + 1)
+            unvisited[square] = False
+
+            # Dijkstra's algorithm ends when the destination square has been visited.
+            if not unvisited[destination]:
+                return distance[destination]
+
+            ud = {k: v for k, v in distance.items() if unvisited[k]}  # Unvisited distances
+            next_square = min(ud, key=ud.get)
+            return dijkstra2_r(game, player, next_square, distance, unvisited, destination)
+
+        return dijkstra2_r(game, player, square, distance, unvisited, destination)
 
 
 class HexBoard:
