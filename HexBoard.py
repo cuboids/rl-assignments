db6dd6228527d1a9ddcb7d9d5afa107239065615
choihungzoeker <66267405+choihungzoeker@users.noreply.github.com>
2021-02-26 02:10:16 +0800

    Update HexBoard.py
    line 150: bugfix for not appending opponent rating history
    line 152-177: create method plot_rating_history()

--- HexBoard.py
+++ HexBoard.py
@@ -147,6 +147,34 @@ class Agent:
         else:
             self.rating, opponent.rating = rate_1vs1(rating2, self.rating)
         self.rating_history.append(self.rating)
+        opponent.rating_history.append(opponent.rating)
+        
+    def plot_rating_history(self, *args):
+        """Plot agents rating history"""
+        game = range(1, len(self.rating_history)+1)
+        mu = [rating.mu for rating in self.rating_history]
+        sigma = [rating.sigma for rating in self.rating_history]
+        ci_lower = [a - b for a, b in zip(mu, sigma)]
+        ci_upper = [a + b for a, b in zip(mu, sigma)]
+        fig, ax = plt.subplots()
+        ax.plot(game, mu, label=self.name, color='k')
+        ax.fill_between(game, ci_lower, ci_upper, alpha=0.1)
+        ax.plot(game, mu, 'o', markersize=3, color='k')
+        # Plot other agents (if any)
+        if not args == ():
+            linestyles = ['--', '.', '-.']
+            players = args
+            for player in players:
+                game = range(1, len(player.rating_history)+1)
+                mu = [rating.mu for rating in player.rating_history]
+                sigma = [rating.sigma for rating in player.rating_history]
+                ci_lower = [a - b for a, b in zip(mu, sigma)]
+                ci_upper = [a + b for a, b in zip(mu, sigma)]
+                ax.plot(game, mu, label=player.name, color='k', linestyle=linestyles[players.index(player)])
+                ax.fill_between(game, ci_lower, ci_upper, alpha=0.1)
+        ax.legend()
+        ax.set_xlabel('Number of games (N)')
+        ax.set_ylabel('Trueskill rating')
 
     def analyse_position(self, game):
         """Let the agent evaluate a position
